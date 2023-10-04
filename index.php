6b27235598273a8df314c45fe0dcd9b590d30a9b
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-10-04 17:39:26 +0200

    support steps 1 à 3

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,33 @@
 </head>
 <body>
     <main>
-        
+        <?php
+            $users = [
+                0 => ['name' => "chris", 'x' => 4, 'y' => 2],
+                1 => ['name' => 'helena', 'x' => 7, 'y' => 8],
+                2 => ['name' => 'lisa', 'x' => 2, 'y' => 4],
+                3 => ['name' => 'mystic', 'x' => 5, 'y' => 3],
+                4 => ['name' => 'tim', 'x' => 1, 'y' => 9],
+            ];
+
+            // foreach ($users as $index => $user) {
+            //     echo $user['name'] . ':' . $user['y'] . '-' . $user['x'] . '<br>';
+            // }
+        ?>
+
+        <?php for ($y=1; $y <= 10; $y++): ?>
+            <div class='row'>
+                <?php for ($x=1; $x <= 10; $x++): ?>
+                    <div class='cell'>
+                        <?php foreach ($users as $index => $user):
+                                if ($user['x'] === $x && $user['y'] === $y): ?>
+                                <img src="/images/<?php echo $user['name']?>.png" alt="photo de <?php echo $user['name']?>">
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        <?php endfor; ?>
     </main>
 </body>
 </html>
